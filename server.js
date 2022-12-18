d3f6078e2660a4ab54994c4ac530c18de700ee1e
Havrushchenko <glitteringpuzzlemusic@gmail.com>
2022-12-18 18:51:40 +0200

    added app.post

--- server.js
+++ server.js
@@ -4,29 +4,46 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 var notes = require('./db/db.json');
-app.use(express.static('public'));
+const { v4: uuidv4 } = require('uuid');
 
 // Parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 
 // Parse incoming JSON data
 app.use(express.json());
-
-// GET/notes return the notes.html file
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/notes.html'))
-);
+app.use(express.static('public'));
 
 // GET return the index.html file
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
+app.get('/', (req, res) => 
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
 
 // GET/api/notes read the db.json file and return all saved notes as JSON.
 app.get('/api/notes', (req, res) =>
   res.json(notes)
 );
 
+// GET/notes return the notes.html file
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+// Receive a new note and add it to the db.json file, and then return the new note to the client.
+app.post('/api/notes', (req, res) => {
+        // Extracted new note from request body.  
+        const newNote = req.body;
+        // Assigned unique id obtained from 'uuid' package
+        newNote.id = uuidv4();
+        // Read data from 'db.json' file
+        let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+        // Pushed new note in notes file 'db.json'
+        data.push(newNote);
+        // Written notes data to 'db.json' file
+        fs.writeFileSync('./db/db.json', JSON.stringify(data));
+        // Send response
+        res.json(data);
+});
+
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+  console.log(`API server now on port ${PORT}!`);
 });
